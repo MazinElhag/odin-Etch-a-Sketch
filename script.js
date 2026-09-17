54cfa7f57260e556706ebf59fce3fcd0f754e96b
Mazin <mazin4elhag@gmail.com>
2026-09-17 14:32:39 +0200

    add teh divs for teh grid in js'

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const container = document.querySelector(".container");
 
+for(let i = 0; i < 4; i++){
+    const line = document.createElement("div");
+    line.className = "line";
+    for(let j = 0; j < 4; j++){
+        const square = document.createElement("div");
+        square.className = "square"
+        line.append(square);
+    }
+    container.append(line);
+}
