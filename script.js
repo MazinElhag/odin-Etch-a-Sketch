1ac30d4c1c5074fcda5054fa3744ba2ad5f15173
Mazin <mazin4elhag@gmail.com>
2026-09-18 12:42:58 +0200

    change the background color of block when hovering

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const container = document.querySelector(".container");
 
-for(let i = 0; i < 4; i++){
+for(let i = 0; i < 16; i++){
     const line = document.createElement("div");
     line.className = "line";
-    for(let j = 0; j < 4; j++){
+    for(let j = 0; j < 16; j++){
         const square = document.createElement("div");
         square.className = "square"
         line.append(square);
     }
     container.append(line);
-}
+} 
+
+
+const squares = document.querySelectorAll(".square");
+
+
+
+squares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+                square.classList.add("background");
+            }
+        )
+    }
+)
