63ba82041d14ad84f30dc0a109e3b7abcaa78edb
Mazin <mazin4elhag@gmail.com>
2026-09-18 17:02:47 +0200

    add new grid button

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
+const body = document.querySelector("body");
 const container = document.querySelector(".container");
 
-for(let i = 0; i < 16; i++){
-    const line = document.createElement("div");
-    line.className = "line";
-    for(let j = 0; j < 16; j++){
-        const square = document.createElement("div");
-        square.className = "square"
-        line.append(square);
+const numberOfSquare = document.createElement("button");
+numberOfSquare.textContent = "new grid";
+
+body.append(numberOfSquare);
+
+numberOfSquare.addEventListener("click", () =>{
+        let ask = prompt("The number of squares per side for the new grid(anything bigger than 100 will count as 100)", "16");
+        container.innerHTML = '';
+        grid(Number(ask));
     }
-    container.append(line);
-} 
+)
 
-
-const squares = document.querySelectorAll(".square");
-
-
-
-squares.forEach((square) => {
+function drawing(squares){
+    squares.forEach((square) => {
         square.addEventListener("mouseover", () => {
                 square.classList.add("background");
             }
         )
+    })
+}
+
+function grid(blockNum=16){
+    if(blockNum > 100)
+        blockNum = 100;
+    for(let i = 0; i < blockNum; i++){
+        const line = document.createElement("div");
+        line.className = "line";
+        for(let j = 0; j < blockNum; j++){
+            const square = document.createElement("div");
+            square.className = "square"
+            line.append(square);
+        }
+        container.append(line);
     }
-)
+
+    drawing(document.querySelectorAll(".square"));
+}
+
+grid();
+
+
+
+
+
+
